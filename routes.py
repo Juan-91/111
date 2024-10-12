99c91c8cf9dfcd991cca57cd65763103127a1abc
Juan Manuel Montiel <Juan.Montiel@sdgku.edu>
2024-10-12 23:11:30 +1030

    #3

--- routes.py
+++ routes.py
@@ -1,13 +1,44 @@
-from flask import Flask
+from flask import(
+    Flask,
+    request
+)
+from app.database import task
 
-app=Flask(__name__)                 #Create an instance of flask class {app is now an object}
+app= Flask(__name__)
+#ReST
+#An architectural design pattern for building network-connected systems.
 
-@app.get("/")                       #Flask decorator to map routes to view functions
-def get_home():                     #Flask view function
-    me={                            #Python dictionary {key-value pairs}
-        "first_name":"Juan",
-        "last_name":"Montiel",
-        "hobbies":"Play chess",
-        "is_online":True
+@app.get("/tasks")
+def get_all_tasks():
+    tasks=task.scan()
+    out={
+        "tasks":tasks,
+        "ok":True
     }
-    return me                       #when you return a dict in Flask it is converted to JSON
+    return out
+
+@app.get("/tasks/<int:pk>/")
+def get_task_by_id(pk):
+    single_task =task.select_by_id(pk)
+    out={
+        "task":single_task,
+        "ok":True
+    }
+    return out
+
+@app.post("/tasks")
+def create_task():
+    task_data=request.json
+    task.insert(task.data)
+    return "", 204
+
+@app.put("/tasks/<int:pk>/")
+def update_task_by_id(pk):
+    task_data=request.json
+    task.update_by_id(task_data, pk)
+    return "", 204
+
+@app.delete("/task/<int:pk>/")
+def delete_task_by_id(pk):
+    task.delete_by_id(pk)
+    return "", 204
